f8b0cac57be16a37ade30379b8e4b2198d68bc1a
abubockorriyad123-commits <abubockorriyad123@gmail.com>
2026-05-09 00:15:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ async def ask_ai(user_id, user_text):
         completion = await loop.run_in_executor(
             None, 
             lambda: client.chat.completions.create(
-                model="google/gemini-2.0-flash-exp:free", # এখানে ফ্রি মডেল ব্যবহার করা হয়েছে
+                model="ring/ring-2.6-1t:free", # এখানে ফ্রি মডেল ব্যবহার করা হয়েছে
                 messages=messages,
             )
         )
